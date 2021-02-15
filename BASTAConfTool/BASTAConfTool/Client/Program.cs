9022e82e9e3245b0b56010fcb005b159e721b933
Christian Weyer <christian.weyer@thinktecture.com>
2021-02-15 13:48:54 +0100

    Added conferences client service

--- BASTAConfTool/BASTAConfTool/Client/Program.cs
+++ BASTAConfTool/BASTAConfTool/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BASTAConfTool.Client.Services;
 
 namespace BASTAConfTool.Client
 {
@@ -18,6 +19,7 @@ namespace BASTAConfTool.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<ConferencesClientService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BASTAConfTool/BASTAConfTool/Client/Services/ConferencesClientService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BASTAConfTool.Shared.DTO;
+
+namespace BASTAConfTool.Client.Services
+{
+    public class ConferencesClientService
+    {
+        private HttpClient _httpClient;
+        private string _conferencesUrl = "https://localhost:5001/api/conferences/";
+
+        public ConferencesClientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<ConferenceOverview>> GetConferencesAsync()
+        {
+            var result = await _httpClient.GetFromJsonAsync<List<ConferenceOverview>>(_conferencesUrl);
+
+            return result;
+        }
+
+        public async Task<ConferenceDetails> GetConferenceDetailsAsync(Guid id)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ConferenceDetails>(_conferencesUrl + id);
+
+            return result;
+        }
+
+    }
+}
